bf547f569ae0d0f1f02e1563a30e1cc03fc95cdc
Elmau1618 <mauricio.nieto@utec.edu.pe>
2021-09-14 03:49:54 -0500

    Update circulararray.h

--- circulararray.h
+++ circulararray.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 template <class T>
@@ -8,6 +9,7 @@ private:
     T *array;
     int capacity;
     int back, front;
+    bool empty = true;
     
 public:
     CircularArray();
@@ -44,7 +46,6 @@ CircularArray<T>::CircularArray(int _capacity)
 {
     this->array = new T[_capacity];
     this->capacity = _capacity;
-    this->front = this->back = -1;
 }
 
 template <class T>
